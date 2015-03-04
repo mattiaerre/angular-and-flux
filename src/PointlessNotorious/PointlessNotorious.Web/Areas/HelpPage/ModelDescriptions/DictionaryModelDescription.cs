a82c210c6788be38bdfc7bffe8851e695ee54a30
mattia richetto <mattia.richetto@grsistemi.com>
2015-03-04 16:31:56 +0000

    1st commit

---
+++ src/PointlessNotorious/PointlessNotorious.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PointlessNotorious.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/PointlessNotorious/PointlessNotorious.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PointlessNotorious.Web.WebApiApplication" Language="C#" %>
