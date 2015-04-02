5deb0916e9df1f739ee49a98c1a7a6ab8c5b08a3
mattiaerre <mattia.richetto@gmail.com>
2015-04-02 16:40:41 +0100

    integration w/ require

---
+++ src/StreamingKnife/StreamingKnife.Web/client/app/common/main.js
@@ -0,0 +1,27 @@
+﻿// see: http://stackoverflow.com/questions/8315088/prevent-requirejs-from-caching-required-scripts
+require.config({
+    paths: {
+        angular: '//code.angularjs.org/1.3.14/angular'
+    },
+    //urlArgs: 'bust=' + (new Date()).getTime(),
+    shim: {
+        angular: {
+            exports: 'angular'
+        }
+    },
+});
+
+require(['angular', 'common'], function (angular) {
+    var appName = 'streaming-knife';
+    angular.module(appName, []);
+    angular.module(appName).service('dispatcher', Common.Dispatcher);
+    angular.module(appName).service('logger', Common.Logger);
+    angular.module(appName).controller('TextArea', Common.TextArea);
+
+    angular.module(appName).run(bootstrap);
+
+    bootstrap.$inject = ['logger'];
+    function bootstrap(logger) {
+        logger.init();
+    }
+});
